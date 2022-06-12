b8aaafc772642f4b6b94b8f3c8335e92462caaeb
Margo <ekimargarita@gmail.com>
2022-06-12 17:55:18 +0300

    finish update styles

--- client/src/components/pages/MoviesPage/PanelFilter/PanelFilter.js
+++ client/src/components/pages/MoviesPage/PanelFilter/PanelFilter.js
@@ -1,21 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { Filter } from "./Filter/Filter";
 import { Search } from "./Search/Search";
 
 export const PanelFilter = (props) => {
     const { dataForCards, setValueOption, setSearchToggle, setFilteredMovieCard } = props
-    const [cardsMenu, setCardsMenu] = useState(false)
-
-    const handleClick = () => { 
-        setCardsMenu(!cardsMenu)
-    }   
 
     return (
         <div className="main__filter-panel filter-panel">
             <div className="filter-panel__container">
                 <Filter dataForCards={dataForCards} setValueOption={setValueOption} />
                 <Search dataForCards={dataForCards} setSearchToggle={setSearchToggle} setFilteredMovieCard={setFilteredMovieCard} />
-                {/* <div onClick={() => handleClick()} className={`filter-panel__cards-menu${cardsMenu ? " columnMenu" : ""}`}></div> */}
             </div>
         </div>
     )
